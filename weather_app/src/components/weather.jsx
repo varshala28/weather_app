065360f7e007df42e4e74955b5839bac1d76f5f8
varshala28 <2020sp189@univ.jfn.ac.lk>
2025-02-12 11:52:51 +0530

    Add open weather API

--- weather_app/src/components/weather.jsx
+++ weather_app/src/components/weather.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import './weather.css'
-import search from '../assets/search.png'
+import search_icon from '../assets/search.png'
+import clear from '../assets/clear.png'
+import cloud from '../assets/cloud.png'
+import drizzle from '../assets/drizzle.png'
+import humidity from '../assets/humidity.png'
+import rain from '../assets/rain.png'
+import snow from '../assets/snow.png'
+import wind from '../assets/wind.png'
+import { useEffect } from 'react'
 
 const weather = () => {
+
+    const search =async (city)=>{
+      try{
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}&units=metric`;
+
+        const response=await fetch(url);
+        const data=await response.json();
+        console.log(data);
+      }catch(error){
+
+      }
+    };
+
+    useEffect(() =>{
+      search("London");
+    },[])
+
   return (
     <div className='weather'>
         <div className='search_bar'>
             <input type="text" placeholder='search'/>
-            <img src={search} alt="" />
+            <img src={search_icon} alt="" />
+        </div>
+        <img src={clear} alt="" className='weather-icon'/>
+        <p className ='temp'> 16c</p>
+        <p className ='loc'> london</p>
+        <div className="weather-data">
+          <div className="col">
+            <img src={humidity} alt="" />
+            <div>
+              <p> 81%</p>
+              <span> Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <img src={wind} alt="" />
+            <div>
+              <p> 3.6km/h</p>
+              <span> Wind Speed</span>
+            </div>
+          </div>
         </div>
     
     </div>
